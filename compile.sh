cff8fb40a78448568015985f916d6ac1dced139b
ole00 <ole00@post.cz>
2023-04-07 18:44:35 +0100

    PC app compilation scripts: use NO_CLOSE
    Force serial port not to be closed.

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-gcc -g3 -O0 -o afterburner src_pc/afterburner.c
+gcc -g3 -O0 -DNO_CLOSE -o afterburner src_pc/afterburner.c
--- compile_win64.sh
+++ compile_win64.sh
@@ -1,4 +1,4 @@
 # path to your Win64 cross-compiler
 CC=~/opt/mingw64/bin/x86_64-w64-mingw32-gcc
 
-$CC -g3 -O0 -o afterburner_w64.exe src_pc/afterburner.c -D_USE_WIN_API_
+$CC -g3 -O0 -o afterburner_w64.exe src_pc/afterburner.c -D_USE_WIN_API_ -DNO_CLOSE
